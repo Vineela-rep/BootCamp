8a048dd8653a97e3be0ac4c7de3f8d76e9738fb5
Vineela-rep <77935110+Vineela-rep@users.noreply.github.com>
2022-07-07 23:00:41 +0530

    Add files via upload

---
+++ day2_1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var m = make(map[string]int,26)
+func Frequency(word string,wg *sync.WaitGroup)  {
+	for i:=0;i<len(word);i++ {
+		m[string(word[i])]+= 1
+	}
+	wg.Done()
+}
+func main()  {
+	var wg sync.WaitGroup
+	words := []string{"quick", "brown", "fox", "lazy", "dog"}
+	for i:= range words{
+		wg.Add(1)
+		go Frequency(words[i],&wg)
+	}
+	wg.Wait()
+	fmt.Println(m)
+}

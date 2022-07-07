13d27283b398f1020ce90e8a9330797e6fedaca2
Vineela-rep <77935110+Vineela-rep@users.noreply.github.com>
2022-07-08 00:08:37 +0530

    Update day1_1.go

--- day1_1.go
+++ day1_1.go
@@ -26,14 +26,14 @@ func (m Matrix) SetElements(i,j,num int) (error) {
 	return nil
 }
 
-func (m Matrix) Add(mat2 [][] int)([][] int){
+func (m Matrix) Add(mat Matrix)([][] int){
 	var i,j int
 	for i=0;i<m.GetRows();i++ {
 		for j = 0; j < m.GetColoumns(); j++ {
-			mat2[i][j] = m.elements[i][j] + mat2[i][j];
+			mat.elements[i][j] = m.elements[i][j] + mat.elements[i][j];
 		}
 	}
-	return mat2
+	return mat.elements
 }
 
 func main(){
@@ -42,9 +42,7 @@ func main(){
 	fmt.Println(m.GetRows())
 	fmt.Println(m.GetColoumns())
 	fmt.Println((m.SetElements(1,1,25)))
+	fmt.Println(m.Add(Matrix{rows:2,columns:2,elements:[][]int{{1,1},{1,1}}}))
 
-	var AddMatrix=[][]int{{1,1}, {1,1}}
-	AddMatrix=m.Add(AddMatrix)
-	fmt.Println(AddMatrix)
 
 }
